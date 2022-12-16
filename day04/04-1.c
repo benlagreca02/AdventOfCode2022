21b29fe847171ffeaf7f58191c2c52772b43b718
Benjamin LaGreca <benlagreca02@gmail.com>
2022-12-16 15:29:09 -0500

    Day 4: Part 1 complete

--- day04/04-1.c
+++ day04/04-1.c
@@ -2,14 +2,41 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
+bool between(int left, int center, int right){
+    return left <= center && center <= right;
+}
+
+int doesOneContainOther(int min1, int max1, int min2, int max2){
+    
+    // No Overlap
+    if( max1 < min2 || max2 < min1){
+        return 0;
+    }
+    // Perfect Overlap
+    else if( max1 == max2 && min1 == min2){
+        return 1;
+    }
+    // One contains the other
+    else if( 
+            (between(min1, min2, max1) && between(min1, max2, max1)) || 
+            (between(min2, min1, max2) && between(min2, max1, max2))){
+        return 1;
+    }
+    // half and half overlap
+    else{
+        return 0;
+    }
+}
+
 
 
 int main(){
 
 
     FILE *inputFile;
-    // inputFile = fopen("inputSimple.txt", "r");
     inputFile = fopen("input.txt", "r");
+    // inputFile = fopen("input.txt", "r");
     if(inputFile == NULL){
         printf("Couldn't open file!\n");
         return -1;
@@ -19,6 +46,7 @@ int main(){
     size_t lineLength = 0;
     ssize_t numCharsRead = 0;
 
+    int numContains = 0;
     while ((numCharsRead = getline(&lineData, &lineLength, inputFile)) != -1){
         if(lineData[0] == '\n'){
             break;
@@ -38,18 +66,19 @@ int main(){
         size_t range2MinLength = secondDash - comma - 1;
         size_t range2MaxLength = strlen(secondDash) - 1;
 
-
-        // Cases:
-        // No overlap
-        // Perfec overlap L&R are the same
-        // First hangs over left and Second hangs over right
-        // opposite
-        // First completely overlapps second
-        // second overlaps first
+        // atoi() stops trying to cast when it hits a character it can't cast
+        // in our case, this makes life very easy
+        range1Min = atoi(lineData);
+        range1Max = atoi(firstDash);
+        range2Min = atoi(comma);
+        range2Max = atoi(secondDash);
 
 
+
+        numContains += doesOneContainOther(range1Min, range1Max, range2Min, range2Max);
     }
 
+    printf("Answer: %d\n", numContains);
 
     fclose(inputFile);
 
